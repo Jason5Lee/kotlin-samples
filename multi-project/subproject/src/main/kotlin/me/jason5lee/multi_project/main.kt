b520c378bd94c5e7ea2d75a6e43b362534245d7e
Jason5Lee <jason5lee@hotmail.com>
2022-08-04 14:45:02 +0800

    add multi-project

---
+++ multi-project/subproject/src/main/kotlin/me/jason5lee/multi_project/main.kt
@@ -0,0 +1,5 @@
+package me.jason5lee.multi_project
+
+fun main() {
+    println("Hello, world!")
+}
---
+++ multi-project/subproject/src/test/kotlin/me/jason5lee/multi_project/SimpleTest.kt
@@ -0,0 +1,10 @@
+package me.jason5lee.multi_project
+
+import kotlin.test.*
+
+class SimpleTest {
+    @Test
+    fun test() {
+        assertEquals(1, 1)
+    }
+}
